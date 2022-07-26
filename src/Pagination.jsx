6c08ec31ee14c78140b31a340ea77252f8270196
lancero793 <carlos.guzman93@live.com>
2022-07-26 07:15:50 -0500

    Pagination modified

--- src/Pagination.jsx
+++ src/Pagination.jsx
@@ -3,9 +3,11 @@ import Movies from './Movies.json';
 function Pagination(props) {
     const totalMovies = Movies.length;
     const [active, setActive] = useState(1);
+    const moviesPerView = 4;
+    const totalPages = Math.ceil(totalMovies / moviesPerView)
     const getTotalLinks = () => {
         let links = [];
-        for (let i = 1; i <= totalMovies; i++) {
+        for (let i = 1; i <= totalPages; i++) {
             links.push(<a className={i == active ? 'active' : ''} onClick={()=> setActive(i)}>
             {i}
           </a>);
@@ -14,7 +16,7 @@ function Pagination(props) {
     }
   return (
     <div className="pagination2">
-      <span>Page {active} of { totalMovies }:</span>
+      <span>Page {active} of { totalPages }:</span>
       { getTotalLinks() }
     </div>
   );
